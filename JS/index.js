701823d2d2ad6e1a237bf9e85566e378a3fd8de7
Kaylah-Langburt <klangburt17@gmail.com>
2023-02-06 10:41:57 -0500

    Adding JS

---
+++ JS/index.js
@@ -0,0 +1,6 @@
+$(function (){
+    console.log(
+        $(".flower").animate({height: '826px', width: '826px'},"slow")
+        (".flower").animate({height: '150px', width:'150px'}, "slow")
+    )
+})
