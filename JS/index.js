c75a4042f8962ed362231693ac8a2535dc8b7d15
Kaylah-Langburt <klangburt17@gmail.com>
2023-02-07 18:50:12 -0500

    Fixing media query

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,6 @@
 $(function (){
-    console.log(
-        $(".flower").animate({height: '826px', width: '826px'},"slow")
-        (".flower").animate({height: '150px', width:'150px'}, "slow")
-    )
+    var hello = 'hello world';
+    console.log( hello );
+    $(".flower img").animate({height: '826px', width: '826px'},"slow");
+    $(".flower img").animate({height: '150px', width:'150px'}, "slow");
 })
